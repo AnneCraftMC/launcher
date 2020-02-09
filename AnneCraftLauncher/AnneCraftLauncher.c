9a6fd295fd40f6b22ca31be6491f5037d59ac105
Anne <sydneyerickson@icloud.com>
2020-02-09 14:44:08 -0800

    Initial attempt to download the JDK

---
+++ AnneCraftLauncher/AnneCraftLauncher.c
@@ -0,0 +1,11 @@
+﻿// AnneCraftLauncher.cpp : Defines the entry point for the application.
+//
+
+#include "AnneCraftLauncher.h"
+#include "JREDownloader.h"
+
+int main()
+{
+	jre_download();
+	return 0;
+}
