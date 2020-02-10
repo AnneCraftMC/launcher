e380567fde98407ccf02bf79696dd701f4945dc0
Anne Erickson <sydneyerickson@icloud.com>
2020-02-09 16:20:14 -0800

    Lots of CMake patches to make the project build

--- launcher/JREDownloader.c
+++ launcher/JREDownloader.c
@@ -1,11 +1,22 @@
 #include <curl/curl.h>
+#include <zip.h>
+#include <stdio.h>
 
 static size_t jre_curl_write_data(void* ptr, size_t size, size_t nmemb, void* stream) {
 	size_t written = fwrite(ptr, size, nmemb, (FILE*)stream);
 	return written;
 }
 
+int jre_zip_extract_entry(const char *filename, void *arg) {
+    static int i = 0;
+    int n = *(int *)arg;
+    printf("Extracted: %s (%d of %d)\n", filename, ++i, n);
+
+    return 0;
+}
+
 int jre_download() {
+	/* --- Download JRE --- */
 	CURL* curl_handle;
 	FILE* zip;
 
@@ -20,12 +31,17 @@ int jre_download() {
 	zip = fopen("jre.zip", "wb");
 	if (zip) {
 		curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, zip);
+		printf("Downloading OpenJDK8U-jre_x64_windows_hotspot_8u242b08.zip");
 		curl_easy_perform(curl_handle);
 		fclose(zip);
 	}
 
 	curl_easy_cleanup(curl_handle);
 	curl_global_cleanup();
+
+	/* --- Extract JRE --- */
+	int arg = 2;
+	zip_extract("jre.zip", "jre", jre_zip_extract_entry, NULL);
 
 	return 0;
 }
